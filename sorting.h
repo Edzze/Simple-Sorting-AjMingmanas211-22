7750294458acc9528cfbbf7118b2dc99dfaa42bd
Edzze <92595995+Edzze@users.noreply.github.com>
2023-05-31 10:42:20 +0000

    Final Commit

--- sorting.h
+++ sorting.h
@@ -1,55 +1,11 @@
 #include <iomanip>
 
-inline void display(int a[], int n){
+inline void display(int *a, int n){
   int i;
   for(i=0;i<n;i++){
     cout<<setw(5)<<a[i];
   }
   cout<<endl;
-
-}
-
-// selection sort function module in C 
-void selectionSort(int data[], int length) 
-{ 
-	int i, j, m, mi; 
-	for (i = 0; i < length - 1; i++) 
-	{ 
-	   /* find the minimum */ 
-	   mi = i; 
-	   for (j = i+1; j < length; j++) 
-	   { 
-	      if (data[j] < data[mi]) { mi = j; } 
-	   } 
-
-     if(i!=mi) swap(data[i],data[mi]);
-     display(data,length);
-     
-	} 
-} 
-
-
-
-void insertion(int a[],int n){
- int i,j;
-  int new_number= 2; //decendingly
-  int temp;
-  //    int a[N]={5,10,15};
- // your a is sorted
-
-  for(j=n-1;j>=0;j--){
-      if(new_number<a[j]){
-          a[j+1]=a[j];  
-          //5 10 15 15
-          //5 10 10 15
-          //5 5 10 15
-         
-      }
-      else break;
-    }
-   a[j+1] =new_number;
-   // 2 5
-
 
 }
 
@@ -61,24 +17,70 @@ inline void swap(int &a,int &b){
   
 }
 
-void bubbleSort(int a[],int n){
+// selection sort function module in C 
+void selectionSort(int data[], int length) 
+{ 
+	int i, j, m, mi; 
 
-int i,j;
-int sorted;
-// how may pair to compare?
-for(j=1;j<=n-1;j++){
-  sorted=0;
-  for(i=0;i<n-(j);i++) // n-2
-  if(a[i]<a[i+1]){   // a[4] <a[5]
-      swap(a[i],a[i+1]);
-      sorted=1;
-  }
-  if(sorted==0) break;
-  display(a,n);
- }
+  /* TRY TO REMOVE 'm' */
+for (j=0; j<length-1; j++)
+  {
+    //***try removing 'm' ***
+    //m=data[j]; //data
+  mi=j; //index of data
   
+	for (i = j+1; i < length; i++) 
+	{ 
+    //for (j=0;j<length-1;j++)
+	   /* find the minimum */ 
+    if(data[mi]>data[i]) // if current num > the next num
+    {
+      //data[j]=data[i]; // change current minimum
+      mi=i; // change index of current minimum 
+    }
+  }
+     swap(data[j],data[mi]);
+  display(data,length);
+	} 
+  
+  
+} 
+
+void insertion(int a[], int n) 
+{
+    for (int i = 1; i < n; i++) {
+        int value = a[i];
+        int j = i - 1;
+
+        while (j >= 0 && a[j] > value) {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = value;
+        display(a, n);
+        cout << "================" << endl;
+    }
 }
 
+void bubbleSort(int *a,int n)
+{
+  int i,j;
+  int sorted;
 
-
-
+  for(j=0;j<n-1;j++)
+  {
+    sorted = 0;
+    for(i=0;i<n-1;i++)
+    {
+      if (a[i]>a[i+1])
+      {
+        swap(a[i],a[i+1]);
+        sorted = 1;
+      }
+    }
+    display(a,n);
+    if (sorted == 0)
+      break;
+  }
+}
